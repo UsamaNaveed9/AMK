3a11ab5301bc7d8e781b6cb26f941e87df737bae
UsamaNaveed9 <usamanaveed9263@gmail.com>
2022-12-21 06:36:59 +0000

    fect: renamed the issurance doctype to Documents and renewals

--- amk/amk/doctype/documents_and_renewals/documents_and_renewals.js
+++ amk/amk/doctype/documents_and_renewals/documents_and_renewals.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2022, smg and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Insurance', {
+frappe.ui.form.on('Documents and Renewals', {
 	// refresh: function(frm) {
 
 	// }
--- amk/amk/doctype/documents_and_renewals/documents_and_renewals.py
+++ amk/amk/doctype/documents_and_renewals/documents_and_renewals.py
@@ -4,5 +4,5 @@
 # import frappe
 from frappe.model.document import Document
 
-class Insurance(Document):
+class DocumentsandRenewals(Document):
 	pass
